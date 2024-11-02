fa9899a25ae1ac46728f4f7a3e9e38c9c29fb3f4
mayam08 <maya.mas0808@gmail.com>
2024-11-02 18:30:20 +0200

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,60 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+
+public class Main {
+
+	public static int ex14(int[] arr, int i) {
+		if (i==0) 
+			return arr[0];
+
+		return ex14(arr, i-1)+arr[i];
+			
+	}
+
+	public static int ex15(int[] arr, int i) {
+		if (i==0) {
+			if (arr[0]>0)
+				return 1;
+			else
+				return 0;
+		}
+		if (arr[i] > 0)
+			return ex15(arr, i-1)+1;
+		else
+			return ex15(arr, i-1);
+
+	}
+	
+	public static int ex16( int[] arr, int n, int i) {
+		if (i==0) {
+			if (arr[0] == n) 
+				return 0;
+			else
+				return -1;
+		}
+		if (arr[i] == n)
+			return i;
+	
+		return ex16(arr,n,i-1);
+		
+	}
+
+	public static boolean ex17(int[]arr, int i) {
+		if (i ==0)
+			return true;
+		
+		if (arr[i] >arr[i-1])
+			return ex17(arr, i-1);
+		else
+			return false;
+	}
+
+
+	public static void main(String[] args) {
+		
+		
+				
+		
+	}
+		
+			
+
 }
